6e64dec02400815b3f12a94b2296a2ac0c8aa0c4
hasnaaain <hasnain.com110@gmail.com>
2025-08-18 16:25:32 +0500

    Add files via upload

---
+++ 1_first.js
@@ -0,0 +1,19 @@
+// to print the message;
+console.log("hello");
+//declare variable
+let score = 10;
+let Name = "hasnain";
+let x = null;
+let y = undefined;
+let radius = 2.5;
+let pass = true;
+
+console.log(score);
+console.log(Name);
+console.log(x);
+console.log(y);
+console.log(radius);
+console.log(pass);
+//constant
+const pi = 3.1415; //must be inintialized before using
+console.log(pi);
