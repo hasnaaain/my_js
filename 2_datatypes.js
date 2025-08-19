407cee6e712592c6ee0b646408e2a6007a3617ee
hasnaaain <hasnain.com110@gmail.com>
2025-08-19 18:45:15 +0500

    Add files via upload

---
+++ 2_datatypes.js
@@ -0,0 +1,20 @@
+/* there are total 9 datatypes -->7-premitive(fixed or predefined or builtin )  -->2-non premitive (Array &  functions)
+number, string , boolean , undefined, null, bigint, symbol
+*/
+let price = 34;
+/*non premitive datatypes are mainly objects(colleciton of data)
+2 pemitive are (Array & Functions)
+*/
+//object creation usually constant
+const student = {
+  Name: "hasnian",
+  roll: 20,
+  marks: 55,
+  ispass: true,
+};
+console.log(student);
+
+/*1- use double  quotes with  string vlaues
+2- use , comma  at end if variabble
+3- use  colon :  to assign (key &  value pair)
+4- within the {} */
