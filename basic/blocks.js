6bd752431f80a8b5f4cdd28a3989ede67787fb7e
hasnaaain <hasnain.com110@gmail.com>
2025-10-16 11:21:12 +0500

    Add files via upload

---
+++ basic/blocks.js
@@ -0,0 +1,10 @@
+let x=12;
+console.log(x);
+//block
+{
+    let a=4;
+    //let a=34;
+    //you cannot redefine the same variable in the block or scope otherwiese error will appear
+    
+    console.log(a);
+}
