7e7d72616dd306522bf83a8cc84c38574644d21f
hasnaaain <hasnain.com110@gmail.com>
2025-10-18 23:36:37 +0500

    Add files via upload

---
+++ basic/test.js
@@ -0,0 +1,15 @@
+// formula to define min max random value
+const min = 2
+const max = 8
+//formula= (max - min + 1) + min
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+let nAme = "hsnain";
+console.log("my","name","is",nAme);//my name is hsnain
+console.log("my"+"name"+"is"+nAme);//mynameishsnain
+
+
+
